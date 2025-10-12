99a1fa0bf5b2b067ab09b26f14762a82cc365e04
Nafisah Aliyah <nafisahaliyah01@gmail.com>
2025-10-12 21:42:38 +0700

    form

--- form/form_validasi.php
+++ form/form_validasi.php
@@ -1,48 +1,73 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Form Input dengan Validasi</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    </head>
-    <body>
-        <h1>Form Input dengan Validasi</h1>
-        <form id="myForm" method="post" action="proses_validasi.php">
-            <label for="nama">Nama:</label>
-            <input type="text" id="nama" name="nama">
-            <span id="nama-error" style="color: red;"></span><br>
+<head>
+    <title>Form Input dengan Validasi AJAX</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+    <h1>Form Input dengan Validasi AJAX</h1>
+<form id="myForm">
+    <label for="nama">Nama:</label>
+    <input type="text" id="nama" name="nama">
+    <span id="nama-error" style="color: red;"></span><br>
 
-            <label for="email">Email:</label>
-            <input type="text" id="email" name="email">
-            <span id="email-error" style="color: red;"></span><br>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email">
+    <span id="email-error" style="color: red;"></span><br>
 
-            <input type="submit" value="Submit">
-        </form>
-        <script>
-            $(document).ready(function(){
-                $("#myForm").submit(function(event){
-                    var nama = $("#nama").val();
-                    var email = $("#email").val();
-                    var valid = true;
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password">
+    <span id="password-error" style="color: red;"></span><br>
 
-                    if (nama === "") {
-                        $("#nama-error").text("Nama harus diisi.");
-                        valid = false;
-                    } else {
-                        $("#nama-error").text("");
-                    }
+    <input type="submit" value="Submit">
+</form>
 
-                    if (email === "") {
-                        $("#email-error").text("Email harus diisi.");
-                        valid = false;
-                    } else {
-                        $("#email-error").text("");
-                    }
+<div id="response-message" style="margin-top: 20px;"></div>
 
-                    if (!valid) {
-                        event.preventDefault(); // hanya mencegah submit jika tidak valid
-                    }
-                });
+<script>
+$(document).ready(function(){
+    $("#myForm").submit(function(event){
+        event.preventDefault();
+
+        var nama = $("#nama").val().trim();
+        var email = $("#email").val().trim();
+        var password = $("#password").val().trim();
+        var valid = true;
+
+        $("#nama-error, #email-error, #password-error").text("");
+        $("#response-message").text("");
+
+        if (nama === "") {
+            $("#nama-error").text("Nama harus diisi.");
+            valid = false;
+        }
+
+        if (email === "") {
+            $("#email-error").text("Email harus diisi.");
+            valid = false;
+        }
+
+        if (password.length < 8) {
+            $("#password-error").text("Password minimal 8 karakter.");
+            valid = false;
+        }
+
+        if (valid) {
+            $.ajax({
+                url: "proses_validasi.php",
+                type: "POST",
+                data: { nama: nama, email: email, password: password },
+                success: function(response){
+                    $("#response-message").html("<span style='color: green;'>" + response + "</span>");
+                    $("#myForm")[0].reset();
+                },
+                error: function(){
+                    $("#response-message").html("<span style='color: red;'>Terjadi kesalahan saat mengirim data.</span>");
+                }
             });
-        </script>
-    </body>
+        }
+    });
+});
+</script>
+</body>
 </html>
